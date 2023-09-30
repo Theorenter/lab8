cc8fdbbdcc05cd84cc23d437cc83b8c505f03f8f
Артем Теорентер <teorenter@sfedu.ru>
2023-09-30 10:32:54 +0300

    Add timer.pas

---
+++ timer.pas
@@ -0,0 +1,10 @@
+﻿Begin
+  var h := ReadInteger('Введите часы:');
+  assert(h >= 0);
+  var m := ReadInteger('Введите минуты:');
+  assert(m >= 0);
+  var s := ReadInteger('Введите секунды');
+  assert(s >= 0);
+  
+  Println($'Всего секунд: {s + m*60 + h*60*60}');
+end.
