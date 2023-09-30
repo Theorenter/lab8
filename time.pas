453bb2fedec810d959d6d27a2adf1c53428af8b3
Артем Теорентер <teorenter@sfedu.ru>
2023-09-30 10:26:21 +0300

    Add time.pas

---
+++ time.pas
@@ -0,0 +1,8 @@
+﻿Begin
+  var t := ReadInteger('Введите год:');
+  assert(t >= 0);
+  
+  if (t mod 4 = 0) and (t mod 100 <> 0) and (t mod 400 <> 0) then
+    print('Год високосный.')
+  else print('Год невисокосный.')
+end.
