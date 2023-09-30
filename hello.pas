7f07b13353a313fd6f086cbc007aa01f88bbbd86
Артем Теорентер <teorenter@sfedu.ru>
2023-09-30 10:21:56 +0300

    Edit hello.pas

--- hello.pas
+++ hello.pas
@@ -1,3 +1,10 @@
 ﻿Begin
-  Print('Привет, мир!');
+  var t := ReadInteger('Какой сейчас час?');
+  assert((t >= 0) and (t <= 23));
+  
+  if (t >= 4) and (t <= 10) then
+    println('Доброе утро, мир')
+  else if (t >= 11) and (t <= 16) then
+    println('Добрый вечер, мир!')
+  else println('Доброй ночи, мир');
 end.
